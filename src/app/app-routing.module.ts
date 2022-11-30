dd7f5196fa4bb768f2898c2ccb2fb0135bedefbe
Sameer <sameerejaz970@gmail.com>
2022-11-30 12:32:43 +0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'mian',component:MainComponent},
+{path:'',component:MainComponent},
+{path:'admin',component:AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
