ba41f45f7b6755f8e495e3408368847666db3152
Sameer <sameerejaz970@gmail.com>
2022-11-30 12:27:31 +0500

    ok

---
+++ src/app/admin/product-creat/product-creat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-creat',
+  templateUrl: './product-creat.component.html',
+  styleUrls: ['./product-creat.component.css']
+})
+export class ProductCreatComponent {
+
+}
