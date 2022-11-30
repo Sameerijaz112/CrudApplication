ecd04891f11d9bb06eadd799d57fc2c710d29efb
Sameer <sameerejaz970@gmail.com>
2022-11-30 14:43:46 +0500

    Routeroutlet ny kaam kharab keeya tha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MainModule,
+    AdminModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
