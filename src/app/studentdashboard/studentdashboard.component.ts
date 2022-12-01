89647035aae924a22b7eb06361756c93423bafdd
Sameer <sameerejaz970@gmail.com>
2022-12-01 11:06:14 +0500

    binding

--- src/app/studentdashboard/studentdashboard.component.ts
+++ src/app/studentdashboard/studentdashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-studentdashboard',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class StudentdashboardComponent {
 
+
+studentVal!:any
+
+
+
+ constructor(private formbuilder:FormBuilder){}
+
+ ngonit(): void {
+  this.studentVal = this.formbuilder.group({
+    Name:[],
+    Class:[],
+    Email:[],
+    Password:[],
+  })
+ }
 }
