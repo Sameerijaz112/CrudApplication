2653f42d3f2417448bfdf46d160c669b049a9939
Sameer <sameerejaz970@gmail.com>
2022-12-01 11:13:58 +0500

    okok

--- src/app/studentdashboard/studentdashboard.component.ts
+++ src/app/studentdashboard/studentdashboard.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { FormBuilder,FormGroup } from "@angular/forms";
+import { ApiService } from '../shared/api.service';
+import { StudentModel } from './student.model';
 
 @Component({
   selector: 'app-studentdashboard',
   templateUrl: './studentdashboard.component.html',
   styleUrls: ['./studentdashboard.component.css']
 })
-export class StudentdashboardComponent {
-
+export class StudentdashboardComponent { 
 
 studentVal!:any
+studentobj:StudentModel= new StudentModel;
 
 
 
- constructor(private formbuilder:FormBuilder){}
+ constructor(private formbuilder:FormBuilder, private api:ApiService){}
 
  ngonit(): void {
   this.studentVal = this.formbuilder.group({
@@ -22,5 +24,12 @@ studentVal!:any
     Email:[],
     Password:[],
   })
+ }
+
+ Addstudent(){
+this.api.poststudent(this.studentobj).subscribe({next : (v) => {
+  console.log(v)
+} })
+
  }
 }
