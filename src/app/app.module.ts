54bf81c5cee851fb414a4ba20a59591dbc998c20
Sameer <sameerejaz970@gmail.com>
2022-12-01 10:34:54 +0500

    newew

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,12 +13,15 @@ import { StudentdashboardComponent } from './studentdashboard/studentdashboard.c
   declarations: [
     AppComponent,
     StudentdashboardComponent,
+    
 
  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
     
 
   ],
