5e9a32732d5306e921b01ed79e5c5c0fee2b23e7
Sameer <sameerejaz970@gmail.com>
2022-12-01 11:36:35 +0500

    get studet api for frontend

--- src/app/studentdashboard/studentdashboard.component.ts
+++ src/app/studentdashboard/studentdashboard.component.ts
@@ -12,6 +12,7 @@ export class StudentdashboardComponent {
 
 studentVal!:any
 studentobj:StudentModel= new StudentModel;
+studentlist:any =[]
 
 
 
@@ -27,9 +28,31 @@ studentobj:StudentModel= new StudentModel;
  }
 
  Addstudent(){
+  this.studentobj.Name = this.studentVal.value.Name;
+  this.studentobj.Class = this.studentVal.value.Class;
+  this.studentobj.Email = this.studentVal.value.Email;
+  this.studentobj.Password = this.studentVal.value.Password;
+
+
 this.api.poststudent(this.studentobj).subscribe({next : (v) => {
   console.log(v)
-} })
+},
+error: (e) => {
+  console.log(e)
+  alert("error")
+},
+complete:() => {
+  console.log("Student Record Added")
+  alert("Student Record Added")
+  this.getstudent();
+}
+ 
+})
 
+ }
+ getstudent(){
+  this.api.getstudent().subscribe(res => {
+    this.studentlist = res;
+  })
  }
 }
